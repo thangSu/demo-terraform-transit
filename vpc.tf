ed1d7bdadba681313c5e3a308c86da0b0373c6a6
thangSu <97284217+thangSu@users.noreply.github.com>
2023-02-03 14:10:38 +0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -103,7 +103,7 @@ resource "aws_ec2_transit_gateway_route_table" "association_default_route_table"
 resource "aws_ec2_transit_gateway_route" "tgw_default_route" {
   destination_cidr_block         = "0.0.0.0/0"
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgva3.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw.association_default_route_table_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.transit.association_default_route_table_id
 }
 resource "aws_route_table" "rt_nat" {
   vpc_id = aws_vpc.vpc["Egress VPC"].id
